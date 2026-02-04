64943d8f64b32a1cc756635a445fa33d7644721f
Aymeric <aymeric@MacBook-Pro-de-Aymeric.local>
2026-02-04 12:37:32 +0100

    first step

--- src/main/java/com/example/romannumeral/RomanCalculator.java
+++ src/main/java/com/example/romannumeral/RomanCalculator.java
@@ -8,4 +8,11 @@ public class RomanCalculator {
     public String askCesar() {
         return "Cesar said TDD is fun";
     }
+
+    public String translate(int i) {
+        if(i == 2) {
+            return "II";
+        }
+        return "I";
+    }
 }
--- src/test/java/com/example/romannumeral/RomanCalculatorTest.java
+++ src/test/java/com/example/romannumeral/RomanCalculatorTest.java
@@ -1,6 +1,7 @@
 package com.example.romannumeral;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -17,4 +18,20 @@ public class RomanCalculatorTest {
 
         Assertions.assertEquals("Cesar said TDD is fun", cesarAnswer);
     }
+
+    @Test
+    void it_translate_1_to_I() {
+        String result = calculator.translate(1);
+
+        Assertions.assertEquals("I", result);
+    }
+
+    @Test
+    void it_translate_2_to_II() {
+        String result = calculator.translate(2);
+
+        Assertions.assertEquals("II", result);
+    }
+
+
 }
